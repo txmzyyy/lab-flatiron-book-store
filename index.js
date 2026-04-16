967fa8a949e92261265b6cc4fae385afa9be6eb3
txmzyyy <tamaramk08@gmail.com>
2026-04-16 11:21:53 +0300

    code solution

--- index.js
+++ index.js
@@ -44,4 +44,31 @@ const bookStore = {
 }
 
 // Write your code here!
+const bookStoreTitle = document.getElementById("header")
+bookStoreTitle.textContent = bookStore.name
 
+const bookList = document.getElementById("book-list");
+
+bookStore.books.forEach(book => {
+    const bookContainer = document.createElement("li");
+
+    const bookTitle = document.createElement("h3");
+    bookTitle.textContent = book.title;
+
+    const bookAuthor = document.createElement("p");
+    bookAuthor.textContent = `Author: ${book.author}`;
+    
+    const bookImage = document.createElement("img");
+    bookImage.src = book.imageUrl;
+
+    bookContainer.appendChild(bookTitle);
+    bookContainer.appendChild(bookAuthor);
+    bookContainer.appendChild(bookImage);
+
+    bookList.appendChild(bookContainer);
+});
+
+const deleteThis = document.getElementById("delete-this");
+if (deleteThis) {
+    deleteThis.remove();
+}
